93cd43ecd4495173cfbf68e92e8283bd6597b4a2
nicksymbiosispit <nickiscool230@gmail.com>
2026-08-30 19:25:13 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 /* Use only your browser-safe Supabase publishable/anon key here. */
-export const SUPABASE_URL = 'https://urfzmssjizfjjetjsuvv.supabase.co/rest/v1/';
+export const SUPABASE_URL = 'https://urfzmssjizfjjetjsuvv.supabase.co';
 export const SUPABASE_KEY = 'sb_publishable_5NCTZ5F6S043ZIfDGeAFkg_dwxEoNPx';
